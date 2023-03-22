733202d9a5031b22b7658d3df6cec32a5f777280
livia-macon <livia.s.macon@gmail.com>
2023-03-22 15:50:54 -0300

    FIX: Small tweaks to methods and change in the return type of the find process

--- botcity/core/bot.py
+++ botcity/core/bot.py
@@ -7,14 +7,14 @@ import random
 import subprocess
 import time
 import webbrowser
-from typing import Union
-from typing import Generator
+from typing import Union, List
 
 
 import pyperclip
 from botcity.base import BaseBot, State
 from botcity.base.utils import is_retina, only_if_element
 from PIL import Image, ImageGrab
+from psutil import Process
 from pynput.keyboard import Controller as KbController
 from pynput.keyboard import Key
 from pynput.mouse import Controller as MouseController
@@ -446,7 +446,7 @@ class DesktopBot(BaseBot):
         return self.find_until(label, x, y, width, height, threshold=threshold, matching=matching,
                                waiting_time=waiting_time, best=best, grayscale=True)
 
-    def find_process(self, name: str = None, pid: str = None) -> Generator:
+    def find_process(self, name: str = None, pid: str = None) -> List[Process]:
         """
         Find a process by name or PID
 
@@ -455,34 +455,28 @@ class DesktopBot(BaseBot):
             pid (str) or (int): The PID (Process Identifier).
 
         Return:
-            generator (Generator): The Generator Object.
+            process (psutil.Process): A Process instance.
         """
         for process in psutil.process_iter():
             try:
-                if (name is not None and process.name() == name) or \
+                if (name is not None and name in process.name()) or \
                         (pid is not None and process.pid == pid):
-                    yield process
+                    return process
             except (psutil.NoSuchProcess, psutil.AccessDenied):
                 pass
         return None
 
-    def terminate_process(self, generator: Generator):
+    def terminate_process(self, process: Process):
         """
-        Terminate the process via the received generator object.
+        Terminate the process via the received Process object.
 
         Args:
-            generator (Generator): The Generator Object.
+            process (psutil.Process): The process to terminate.
         """
-        for object in generator:
-            objs = object.__dict__
-            pid = objs.get("_pid")
-
-            process = psutil.Process(pid)
-            process.terminate()
-
-            psutil.wait_procs(generator, timeout=10)
-            if process.is_running() is True:
-                raise Exception("Terminate process failed")
+        process.terminate()
+        process.wait(10)
+        if process.is_running():
+            raise Exception("Terminate process failed")
 
     def get_last_element(self):
         """
