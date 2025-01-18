2dbef9395de41988251fe46edfaa4ef95b20fff6
welli7ngton <welli7ngton@zorin>
2025-01-18 17:22:27 -0300

    fix: typeerror

--- botcity/core/bot.py
+++ botcity/core/bot.py
@@ -332,7 +332,7 @@ class DesktopBot(BaseBot):
         waiting_time: int = 10000,
         best: bool = True,
         grayscale: bool = False,
-    ) -> Tuple[int, int, int, int] | None:
+    ) -> Union[Tuple[int, int, int, int], None]:
         """
         Find an element defined by label on screen until a timeout happens.
 
